d39808d1fa766386ee16c49ea93d6eaf273215c0
HocineTouati <htouati1@stu.vistula.edu.pl>
2026-05-20 01:15:56 +0100

    Initial commit

---
+++ src/main/java/pl/edu/vistula/first_project_java/controller/HelloController.java
@@ -0,0 +1,31 @@
+package pl.edu.vistula.first_project_java.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/")
+    public String home() {
+
+        return "redirect:/greeting";
+    }
+
+    @GetMapping("/greeting")
+    public String greeting(
+            @RequestParam(
+                    name = "name",
+                    required = false,
+                    defaultValue = "World"
+            ) String name,
+            Model model
+    ) {
+
+        model.addAttribute("name", name);
+
+        return "greeting";
+    }
+}
